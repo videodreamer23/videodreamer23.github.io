1b82d10362e1972f804a79d33efc6fb447430df1
VideoDreamer <dreamstudio2023@126.com>
2023-11-07 14:10:17 +0800

    videodreamer

--- convert.py
+++ convert.py
@@ -11,13 +11,14 @@ def mp4_to_gif(mp4_path, gif_path):
 # 指定MP4文件路径和GIF文件路径
 # in_path_root = ".\static\\compare_examples\\videos"
 # out_path_root = ".\static\\compare_examples\\gifs"
-in_path_root = "..\..\changed_video"
-out_path_root = "..\..\changed_video"
+in_path_root = ".\static\\all_examples\gifs\\51.mp4"
+out_path_root = ".\static\\all_examples\gifs\\51.gif"
 
+mp4_to_gif(in_path_root, out_path_root)
 
-input_dirs = os.listdir(in_path_root)
-for name in input_dirs:
-    order,_ = name.split(".")
-    mp4_path = os.path.join(in_path_root,name)
-    gif_path = os.path.join(out_path_root,order+".gif")
-    mp4_to_gif(mp4_path, gif_path)
+# input_dirs = os.listdir(in_path_root)
+# for name in input_dirs:
+#     order,_ = name.split(".")
+#     mp4_path = os.path.join(in_path_root,name)
+#     gif_path = os.path.join(out_path_root,order+".gif")
+#     mp4_to_gif(mp4_path, gif_path)
